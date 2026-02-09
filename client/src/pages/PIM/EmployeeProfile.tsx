3915108d50aff480e1cf47c88c64adb9af3ef56d
Abdul Raheem <abdul.raheem.17.9.2002@gmail.com>
2026-02-09 10:17:18 +0500

    pushed code

--- client/src/pages/PIM/EmployeeProfile.tsx
+++ client/src/pages/PIM/EmployeeProfile.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ChevronLeft, User, Phone, Briefcase, FileText, Download, Edit2 } from 'lucide-react';
 
